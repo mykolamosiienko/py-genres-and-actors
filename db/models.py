11bf7d4be7e3fdb85099e5de101e633664f3b7ca
Mykola Mosiienko <koljamos@gmail.com>
2026-02-23 16:32:03 +0200

    solution3

--- db/models.py
+++ db/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import init_django_orm  # noqa: F401
 
+
 from django.db.models import QuerySet
 from db.models import Genre, Actor
 
